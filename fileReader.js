420c7319883c20a1829c65e32fcc01f5c6199a25
Dan <d.epstein92@gmail.com>
2017-08-05 00:17:33 -0400

    currently working

--- fileReader.js
+++ fileReader.js
@@ -3,28 +3,33 @@ const fs = require('fs');
 
 
 module.exports = {
-  peopleOneReader: (filePath) => { //WARNING ARROW FUNCTION COULD CAUSE ERROR. READ DOC
+  peopleOneReader: (filePath1, filePath2) => { //WARNING ARROW FUNCTION COULD CAUSE ERROR. READ DOC
     return new Promise(function(resolve, reject) {
-      fs.readFile(filePath, 'utf-8', function(err, data) {
+      fs.readFile(filePath1, 'utf-8', function(err, data1) {
+        fs.readFile(filePath2, 'utf-8', (err, data2) => {
+          let sortedData = JSON.parse(data1);
+          let sortedData2 = JSON.parse(data2);
+          let combinedData = sortedData.concat(sortedData2);
         if (err) {
           reject('An error has been thrown: Line25');
         } else {
-          resolve(JSON.parse(data));
+          resolve(combinedData);
         }
       });
     });
-  }
-  peopleTwoReader: (filePath) => {
-    return new Promise(function(resolve, reject) {
-      fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-          reject('An error has occured');
-        } else {
-          resolve(JSON.parse(data));
-        }
-      });
-    })
-  }
+  })
+  // peopleTwoReader: (filePath) => {
+  //   return new Promise(function(resolve, reject) {
+  //     fs.readFile(filePath, 'utf-8', (err, data) => {
+  //       if (err) {
+  //         reject('An error has occured');
+  //       } else {
+  //         resolve(JSON.parse(data));
+  //       }
+  //     });
+  //   })
+  // }
 
 
 }
+ }
--- index.js
+++ index.js
@@ -5,25 +5,25 @@ const fileReader = require('./fileReader.js');
 
 
 
-fileReader.peopleOneReader('./people1.json').then(function(data) { //Killian, Koen, Ashlee
-  let dataHandler = data.sort();
-  fs.writeFile('sortPeopleList.txt', data, (err) => {
+fileReader.peopleOneReader('./people1.json', './people2.json').then(function(data) { //Killian, Koen, Ashlee
+  let sortedData = data.sort();
+  fs.writeFile('sortPeopleList.txt', sortedData, (err) => {
     if(err){
       console.log('An error has been thrown; Line 10');
     }
       //console.log(parsedData);
-      console.log('successfully executed written file: ' + dataHandler);
+      console.log('successfully executed written file: ' + sortedData);
 
   });
 
 })
 
-fileReader.peopleTwoReader('./people2.json').then(function(data){ // "Sara", "Alex", "Loet", "Thirsa", "Inessa"
-  let dataHandler = data.sort();
-  fs.writeFile('sortPeopleList.txt', data, (err) => {
-    if(err){
-      console.log('An error has occured, thrown at line 25')
-    }
-    console.log('successfully executed written file: ' + dataHandler);
-  })
-})
+// fileReader.peopleTwoReader('./people2.json').then(function(data){ // "Sara", "Alex", "Loet", "Thirsa", "Inessa"
+//   let dataHandler = data.sort();
+//   fs.writeFile('sortPeopleList.txt', data, (err) => {
+//     if(err){
+//       console.log('An error has occured, thrown at line 25')
+//     }
+//     console.log('successfully executed written file: ' + dataHandler);
+//   })
+// })
